53e34d244a4f1024b157c65e6fd087afbf77a9bd
StudentAlleg <studentalleg-pilot@yahoo.com>
2023-10-18 13:49:13 -0700

    update to goo with FinishHandler

--- CM170 Movement Prototype/Assets/Scripts/FinishHandler.cs
+++ CM170 Movement Prototype/Assets/Scripts/FinishHandler.cs
@@ -8,29 +8,31 @@ public class FinishHandler : MonoBehaviour
 {
     DateTime lapStart = DateTime.Now;
 
-    TimeSpan fastestLap = TimeSpan.Zero;
+    TimeSpan fastestLap = TimeSpan.MaxValue;
 
     TimeSpan currentLap = TimeSpan.Zero;
 
     GameObject timer = null;
+
+    TextMeshProUGUI textMeshPro = null;
     // Start is called before the first frame update
     void Start()
     {
         timer = GameObject.Find("Canvas/Time");
-        Debug.Log(timer);
+        textMeshPro = timer.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentLap = lapStart.Subtract(DateTime.Now);
-        timer.GetComponent<TextMeshPro>().SetText(currentLap.ToString() + " | " + fastestLap.ToString());
+        currentLap = DateTime.Now.Subtract(lapStart);
+        textMeshPro.SetText(currentLap.Seconds + " | " + fastestLap.Seconds);
     }
 
     void OnTriggerExit2D() {
         if (currentLap < fastestLap) {
             fastestLap = currentLap;
         }
-
+        lapStart = DateTime.Now;
     }
 }
