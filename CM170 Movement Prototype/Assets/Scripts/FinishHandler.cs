fb15b410997d1c721a1737ad6aaa64437d13eaf9
IcySmoooth <90065902+IcySmoooth@users.noreply.github.com>
2023-10-19 03:11:28 -0700

    Added minimap to new course

--- CM170 Movement Prototype/Assets/Scripts/FinishHandler.cs
+++ CM170 Movement Prototype/Assets/Scripts/FinishHandler.cs
@@ -8,29 +8,33 @@ public class FinishHandler : MonoBehaviour
 {
     DateTime lapStart = DateTime.Now;
 
-    TimeSpan fastestLap = TimeSpan.Zero;
+    TimeSpan fastestLap = TimeSpan.FromMinutes(1);
 
     TimeSpan currentLap = TimeSpan.Zero;
 
     GameObject timer = null;
+
+    TextMeshProUGUI textMeshPro = null;
     // Start is called before the first frame update
     void Start()
     {
         timer = GameObject.Find("Canvas/Time");
-        Debug.Log(timer);
+        textMeshPro = timer.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentLap = lapStart.Subtract(DateTime.Now);
-        timer.GetComponent<TextMeshPro>().SetText(currentLap.ToString() + " | " + fastestLap.ToString());
+        currentLap = DateTime.Now.Subtract(lapStart);
+        textMeshPro.SetText("Lap time " + currentLap.TotalSeconds/* + " | " + fastestLap.TotalSeconds*/);
     }
 
-    void OnTriggerExit2D() {
+    void OnTriggerEnter2D() {
         if (currentLap < fastestLap) {
             fastestLap = currentLap;
         }
-
+        lapStart = DateTime.Now;
+        currentLap = TimeSpan.Zero;
+        Debug.Log("fastest lap " + fastestLap.TotalSeconds);
     }
 }
